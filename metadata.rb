21dde5b69410e0bb9629daa80baebb69c1a355a8
Marco De Bortoli <marco.debo.debortoli@gmail.com>
2013-11-05 18:24:46 +0000

    Restored the loose dependency on the apt and php package as per original repo. Strict dependency can be set via Cheffile on a per project basis

--- metadata.rb
+++ metadata.rb
@@ -8,8 +8,8 @@ version          "0.5.0"
 
 depends "git", ">= 1.0.0"
 depends "yum", ">= 0.8.0"
-depends "apt", "= 1.8.4"
-depends "php", "= 1.1.0"
+depends "apt", ">= 1.8.4"
+depends "php", ">= 1.1.0"
 depends "chef-php-extra"
 
 %w{ ubuntu debian centos redhat fedora }.each do |os|
